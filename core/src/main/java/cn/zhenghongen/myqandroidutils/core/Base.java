2d4c0f5362a688567d0b588b84ef423eaad0e422
郑宏恩 <zhenghongen@apexsoft.com.cn>
2017-07-19 20:47:40 +0800

    long long ago

---
+++ core/src/main/java/cn/zhenghongen/myqandroidutils/core/Base.java
@@ -0,0 +1,7 @@
+package cn.zhenghongen.myqandroidutils.core;
+
+/**
+ * Created by zhenghongen on 16/7/22.
+ */
+public class Base {
+}
---
+++ core/src/main/java/cn/zhenghongen/myqandroidutils/core/utils/LogUtils.java
@@ -0,0 +1,60 @@
+package cn.zhenghongen.myqandroidutils.core.utils;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Log工具类
+ * Created by zhenghongen on 16/7/22.
+ */
+public class LogUtils {
+
+    public static void d(String message, Object... args) {
+        Logger.d(message, args);
+    }
+
+    public static void d(Object object) {
+        Logger.d(object);
+    }
+
+    public static void e(String message, Object... args) {
+        Logger.e(null, message, args);
+    }
+
+    public static void e(Throwable throwable, String message, Object... args) {
+        Logger.e(throwable, message, args);
+    }
+
+    public static void i(String message, Object... args) {
+        Logger.i(message, args);
+    }
+
+    public static void v(String message, Object... args) {
+        Logger.v(message, args);
+    }
+
+    public static void w(String message, Object... args) {
+        Logger.w(message, args);
+    }
+
+    public static void wtf(String message, Object... args) {
+        Logger.wtf(message, args);
+    }
+
+    /**
+     * Formats the json content and print it
+     *
+     * @param json the json content
+     */
+    public static void json(String json) {
+        Logger.json(json);
+    }
+
+    /**
+     * Formats the json content and print it
+     *
+     * @param xml the xml content
+     */
+    public static void xml(String xml) {
+        Logger.xml(xml);
+    }
+}
